b83aeb920d5d7a831dd8679c267f6b2b5b6eed04
yukienomiya <yn.yuki98@gmail.com>
2020-02-21 00:36:57 +0100

    Delete other files

--- metodologie2019_homework3/src/it/uniroma1/metodologie2019/hw3/WordNetMapping.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma1.metodologie2019.hw3;
-
-import java.util.Optional;
-
-public class WordNetMapping {
-  public Optional<SynsetPairing> getMapping(Synset s) {
-    throw new java.lang.UnsupportedOperationException("Not supported yet.");
-  }
-}
--- metodologie2019_homework3/src/it/uniroma1/metodologie2019/hw3/package-info.java
+++
@@ -1,5 +0,0 @@
-package it.uniroma1.metodologie2019.hw3;
-
-/**
- * The package contains the third homework of the course.
- */
